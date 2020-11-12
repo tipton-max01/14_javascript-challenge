b069c54f52b7fdf5af9b109e5ee9b91f385db061
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-12 13:29:30 -0500

    console-logged each obj with forEach

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,3 +10,8 @@ console.log(data);
 
 // Append one table row `tr` to the table body
 var row = tbody.append("tr");
+
+// Step 1: Loop Through `data` and console.log each data object
+data.forEach(function(ufo) {
+    console.log(ufo);
+  });
