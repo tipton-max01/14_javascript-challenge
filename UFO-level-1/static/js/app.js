07986373f33ec67ab96e0dada8287de609d5000d
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-24 00:29:36 -0500

    revisions: use object.entries to log each ufo value

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,8 +14,10 @@ data.forEach(function(ufo) {
 // Step 2:  Use d3 to append one table row `tr` for each ufo object
 // Don't worry about adding cells or text yet, just try appending the `tr` elements.
   var row = tbody.append("tr");
-// Step 3:  Use `Object.entries` to console.log each ufo value
+// Step 3:  Use `Object.entries` to console.log each ufo value:
   Object.entries(ufo).forEach(function([key, value]) {
     console.log(key, value);
   });
+// Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
+
 });
