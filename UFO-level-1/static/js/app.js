21aed5b3ab523c4f51884203a6e38c7f6cd9ec5e
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-24 00:26:35 -0500

    revisions: Use d3 to append one table row

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,10 +8,14 @@ var tbody = d3.select("tbody");
 //console.log our data.js
 console.log(data);
 
-// Append one table row `tr` to the table body
-var row = tbody.append("tr");
-
-// Step 1: Loop Through `data` and console.log each data object
+// Step 1: Loop Through `data` and console.log each ufo object
 data.forEach(function(ufo) {
-    console.log(ufo);
+  console.log(ufo);
+// Step 2:  Use d3 to append one table row `tr` for each ufo object
+// Don't worry about adding cells or text yet, just try appending the `tr` elements.
+  var row = tbody.append("tr");
+// Step 3:  Use `Object.entries` to console.log each ufo value
+  Object.entries(ufo).forEach(function([key, value]) {
+    console.log(key, value);
   });
+});
